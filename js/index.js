06bdcc5231d7e10c40d758b29d44fd920108f878
Nakatai-0322 <nakatai0322@gmail.com>
2021-12-05 16:31:08 +0900

    とりま成功ビルド。

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 let accessfrom = "アクセス元ページ:" + location.href;
 let resolution = "解像度:" + screen.width + "x" + screen.height;
 
-document.getElementById("out1").innerHTML = accessfrom;
-document.getElementById("out2").innerHTML = resolution;
+window.onload = function onLoad() {
+    target = document.getElementById("out1");
+    target.innerHTML = accessfrom;
+}
