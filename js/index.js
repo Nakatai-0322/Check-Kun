8e05f401d2cbbe82797bb17e07def2cc5c2f1888
Nakatai-0322 <nakatai0322@gmail.com>
2022-01-05 10:35:33 +0900

    ブラウザエージェント情報の追加

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 let accessfrom = "アクセス元ページ:" + location.href;
 let resolution = "解像度:" + screen.width + "x" + screen.height;
+let browser = "ブラウザー: " + window.navigator.userAgent + "   (ver." + window.navigator.appVersion + " )"
 
 window.onload = function onLoad() {
     document.getElementById("out1").innerHTML = accessfrom;
     document.getElementById("out2").innerHTML = resolution;
+    document.getElementById("out3").innerHTML = browser;
 }
