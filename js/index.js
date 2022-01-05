ee76b52f7a9dea7bb7425a49441259c25e92aed1
Nakatai-0322 <nakatai0322@gmail.com>
2022-01-05 10:36:45 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 let accessfrom = "アクセス元ページ:" + location.href;
-let resolution = "解像度:" + screen.width + "x" + screen.height;
+let resolution = "解像度:" + screen.width + " x " + screen.height;
 let browser = "ブラウザー: " + window.navigator.userAgent + "   (ver." + window.navigator.appVersion + " )"
 
 window.onload = function onLoad() {
