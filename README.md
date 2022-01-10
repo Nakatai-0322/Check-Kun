2ea985d3e9f38d7f8b7a1ae9c48a2f323fc2de5c
Nakatai-0322 <nakatai0322@gmail.com>
2022-01-10 18:03:03 +0900

    SomeFix

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
+<!--
+Random-No
+
+Copyright (c) 2022~Present Nakatai.
+
+This software is released under the MIT License.
+https://opensource.org/licenses/mit-license.php
+-->
+
 # Check-Kun
+
+アクセス環境の確認を行えます。
+
+## 使用ライブラリ
+
+* [platform.js](https://github.com/bestiejs/platform.js)
+* [bootstrap](https://getbootstrap.jp)
+
+## 作成者
+
+* [Nakatai](https://github.com/Nakatai-0322)
+* [Contributors](https://github.com/Nakatai-0322/Check-Kun/graphs/contributors)
--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
+/**
+Random-No
+
+Copyright (c) 2022~Present Nakatai.
+
+This software is released under the MIT License.
+https://opensource.org/licenses/mit-license.php
+*/
+// 数値代入部分
 const accessfrom = "アクセス元ページ:" + location.href;
 const resolution = "解像度:" + screen.width + " x " + screen.height;
 const browser = "ブラウザー: " + platform.name + " " + platform.version;
-const os = "OS: " + platform.os.family+ " " + platform.os.version;
+const os = "OS: " + " " + platform.os.family+ " " + platform.os.version;
 
 window.onload = function onLoad() {
     document.getElementById("out1").innerHTML = accessfrom;
