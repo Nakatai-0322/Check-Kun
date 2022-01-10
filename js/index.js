211e421f34df5bc6e3a24387717e54d826e5b2c4
Nakatai-0322 <nakatai0322@gmail.com>
2022-01-10 18:05:07 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ https://opensource.org/licenses/mit-license.php
 const accessfrom = "アクセス元ページ:" + location.href;
 const resolution = "解像度:" + screen.width + " x " + screen.height;
 const browser = "ブラウザー: " + platform.name + " " + platform.version;
-const os = "OS: " + " " + platform.os.family+ " " + platform.os.version;
+const os = "OS: " + platform.os.family+ " " + platform.os.version;
 
 // ページロード時に"out*"に対し代入
 window.onload = function onLoad() {
