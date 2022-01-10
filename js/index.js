d6f0c4d6c21525c402de34f60a05597763f05b4c
Nakatai-0322 <nakatai0322@gmail.com>
2022-01-10 18:06:25 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ This software is released under the MIT License.
 https://opensource.org/licenses/mit-license.php
 */
 // 数値代入部分
-const accessfrom = "アクセス元ページ:" + location.href;
+const accessfrom = "アクセス元ページ: " + location.href;
 const resolution = "解像度:" + screen.width + " x " + screen.height;
 const browser = "ブラウザー: " + platform.name + " " + platform.version;
 const os = "OS: " + platform.os.family+ " " + platform.os.version;
