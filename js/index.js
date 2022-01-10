8caae005bf1833114574a4897054efc07fc0db11
Nakatai-0322 <nakatai0322@gmail.com>
2022-01-10 17:40:39 +0900

    init

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 const accessfrom = "アクセス元ページ:" + location.href;
 const resolution = "解像度:" + screen.width + " x " + screen.height;
-const browser = "ブラウザー: " + window.navigator.userAgent + "   (ver." + window.navigator.appVersion + " )"
+const browser = "ブラウザー: " + platform.name + " " + platform.version;
+const os = "OS: " + platform.os.family+ " " + platform.os.version;
 
 window.onload = function onLoad() {
     document.getElementById("out1").innerHTML = accessfrom;
     document.getElementById("out2").innerHTML = resolution;
     document.getElementById("out3").innerHTML = browser;
+    document.getElementById("out4").innerHTML = os
 }
