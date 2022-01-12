ef92a312ffa4f24d2e08d0493b5d6492ddd5fa4d
Nakatai-0322 <nakatai0322@gmail.com>
2022-01-12 18:53:05 +0900

    JS側からHTML側に変更

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ This software is released under the MIT License.
 https://opensource.org/licenses/mit-license.php
 */
 // 数値代入部分
-const accessfrom = "アクセス元ページ: " + location.href;
-const resolution = "解像度:" + screen.width + " x " + screen.height;
-const browser = "ブラウザー: " + platform.name + " " + platform.version;
-const os = "OS: " + platform.os.family+ " " + platform.os.version;
+const accessfrom = location.href;
+const resolution = screen.width + " x " + screen.height;
+const browser = platform.name + " " + platform.version;
+const os = platform.os.family+ " " + platform.os.version;
 
 // ページロード時に"out*"に対し代入
 window.onload = function onLoad() {
