051f49c5e3dfc1cead48c4b1e86003830f47bce3
Nakatai-0322 <nakatai0322@gmail.com>
2022-01-29 17:49:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ Copyright (c) 2022~Present Nakatai.
 This software is released under the MIT License.
 https://opensource.org/licenses/mit-license.php
 */
+//"platform.js"をインポート
 import("./lib/platform.js")
 
-// ページロード時に"out*"に対し代入
+//ページロード時に"out"に対し代入
 window.onload = function onLoad() {
     document.getElementById("accessfrom").innerHTML = location.href;
     document.getElementById("resolution").innerHTML = screen.width + " x " + screen.height;
