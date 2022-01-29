e330ed5c75e7081e191c6bbfadcc7d42badc6f51
Nakatai-0322 <nakatai0322@gmail.com>
2022-01-29 17:27:28 +0900

    libのインポートにimport文を使用

--- index.js
+++ index.js
@@ -6,16 +6,12 @@ Copyright (c) 2022~Present Nakatai.
 This software is released under the MIT License.
 https://opensource.org/licenses/mit-license.php
 */
-// 数値代入部分
-const accessfrom = location.href;
-const resolution = screen.width + " x " + screen.height;
-const browser = platform.name + " " + platform.version;
-const os = platform.os.family + " " + platform.os.version;
+import("./lib/platform.js")
 
 // ページロード時に"out*"に対し代入
 window.onload = function onLoad() {
-    document.getElementById("accessfrom").innerHTML = accessfrom;
-    document.getElementById("resolution").innerHTML = resolution;
-    document.getElementById("browser").innerHTML = browser;
-    document.getElementById("os").innerHTML = os
+    document.getElementById("accessfrom").innerHTML = location.href;
+    document.getElementById("resolution").innerHTML = screen.width + " x " + screen.height;
+    document.getElementById("browser").innerHTML = platform.version;
+    document.getElementById("os").innerHTML = platform.os;
 }
