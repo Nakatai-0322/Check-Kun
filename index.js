cb4946d179b12dc2cccaf097dff07054f3c46801
Nakatai-0322 <nakatai0322@gmail.com>
2022-01-30 18:43:33 +0900

    「レンダリングエンジン」の追加

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ https://opensource.org/licenses/mit-license.php
 //"platform.js"をインポート
 import("./lib/platform.js")
 
-
-
 //ページロード時に各部分に対し代入
 window.onload = function onLoad() {
     const accessfrom = "アクセス元ページ: " + location.href;
     const resolution = "解像度: " + screen.width + " x " + screen.height;
-    const browser = "ブラウザ: " + platform.version;
+    const browser = "ブラウザ: " + platform.name + " " + platform.version;
+    const engine = "HTMLレンダリングエンジン: " + platform.layout;
     const os = "OS: " + platform.os;
 
     document.getElementById("accessfrom").innerHTML = accessfrom;
     document.getElementById("resolution").innerHTML = resolution;
     document.getElementById("browser").innerHTML = browser;
     document.getElementById("os").innerHTML = os;
+    document.getElementById("engine").innerHTML = engine;
 }
