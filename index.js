ed835c2df7d291efaf55b3e8ab38665cdde9e35f
Nakatai-0322 <nakatai0322@gmail.com>
2022-03-03 18:36:12 +0900

    仮で実装

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ Copyright (c) 2022~Present Nakatai.
 This software is released under the MIT License.
 https://opensource.org/licenses/mit-license.php
 */
-//"platform.js"をインポート
+
 import ("./lib/platform.js");
+import ("//cdn.jsdelivr.net/npm/axios/dist/axios.min.js")
 
 function Dainyu() {
+    $.get("https://ipinfo.io", function(res) {
+        console.log(res.ip);
+    }, "jsonp");
+
+    const ip = "IPアドレス: " + "<code>" + getIpClient() + "</code>"
     const accessfrom = "アクセス元ページ: " + "<code>" + location.href + "</code>";
     const resolution = "解像度: " + "<code>" + screen.width + " x " + screen.height + "</code>";
     const browser = "ブラウザ: " + "<code>" + platform.name + " " + platform.version + "</code>";
@@ -17,6 +23,7 @@ function Dainyu() {
     const os = "OS: " + "<code>" + platform.os.family + " " + platform.os.version + "</code>";
     const fordevua = "ユーザーエージェント（開発者向け）: " + "<code>" + platform.ua + "</code>";
 
+    document.getElementById("ip").innerHTML = ip;
     document.getElementById("accessfrom").innerHTML = accessfrom;
     document.getElementById("resolution").innerHTML = resolution;
     document.getElementById("browser").innerHTML = browser;
