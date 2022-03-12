e3a7725faad7c6f25087b8d5d3eb2f5e2977ab4e
Nakatai-0322 <nakatai0322@gmail.com>
2022-03-12 19:25:08 +0900

    Update pfmchk.js

--- js/pfmchk.js
+++ js/pfmchk.js
@@ -6,8 +6,7 @@ Copyright (c) 2022~Present Nakatai.
 This software is released under the MIT License.
 https://opensource.org/licenses/mit-license.php
 */
-const platformjspath = "//cdn.jsdelivr.net/npm/platform@latest/platform.min.js"
-import (platformjspath);
+import ("//cdn.jsdelivr.net/npm/platform@latest/platform.min.js");
 
 let rawip = ""
 
@@ -19,12 +18,36 @@ function callback(data) {
 
 function Dainyu() {
     const ip = `IPアドレス: <code>${rawip}</code>`;
-    const accessfrom = `アクセス元ページ: <code>${location.href}</code>`;
-    const resolution = `解像度: <code>${screen.width} x ${screen.height}</code>`;
-    const browser = `ブラウザ: <code>${platform.name} ${platform.version}</code>`;
-    const engine = `HTMLレンダリングエンジン: <code>${platform.layout}</code>`;
-    const os = `OS: <code>${platform.os.family} ${platform.os.version}</code>`;
-    const fordevua = `ユーザーエージェント（開発者向け）: <code>${platform.ua}</code>`;
+    const accessfrom = `
+	アクセス元ページ: 
+	<code>
+		${location.href}
+	</code>`;
+    const resolution = `
+	解像度: 
+	<code>
+		${screen.width} x ${screen.height}
+	</code>`;
+    const browser = `
+	ブラウザ: 
+	<code>
+		${platform.name} ${platform.version}
+	</code>`;
+    const engine = `
+	HTMLレンダリングエンジン: 
+	<code>
+		${platform.layout}
+	</code>`;
+    const os = `
+	OS: 
+	<code>
+		${platform.os.family} ${platform.os.version}
+	</code>`;
+    const fordevua = `
+	ユーザーエージェント（開発者向け）: 
+	<code>
+		${platform.ua}
+	</code>`;
 
     document.getElementById("ip").innerHTML = ip;
     document.getElementById("accessfrom").innerHTML = accessfrom;
