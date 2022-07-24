65859ef90298f0696a4193177b2635e709b79c69
Nakatai-0322 <nakatai0322@gmail.com>
2022-07-24 20:43:43 +0900

    init vue

---
+++ check-kun/src/components/CheckKun.vue
@@ -0,0 +1,38 @@
+<template>
+    <h1 class="text-info my-4">
+        アクセス環境表示ツール
+    </h1>
+    <p>
+        アクセスされている環境を表示します。
+        <br>
+        <strong>
+            サーバー側に情報は流出しません。
+        </strong>
+    </p>
+    <noscript>
+        <p class="plzusejs">
+            このサイトでは<a href="https://w.wiki/3EDd">JavaScript</a>を使用しています。<br>
+            当サイトのコンテンツを表示するには、JavaScriptを有効にしてください。
+        </p>
+    </noscript>
+    <form mb="4" id="form">
+        <input type="button" class="btn btn-outline-danger active" value="表示更新" onclick="Dainyu();">
+    </form>
+    <div class="out">
+        <div id="1line"></div>
+        <div id="ip"></div>
+        <div id="accessfrom"></div>
+        <div id="resolution"></div>
+        <div id="browser"></div>
+        <div id="os"></div>
+        <div id="engine"></div>
+        <div id="fordevua"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: CheckKun
+}
+</script>
+
---
+++ check-kun/src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer>
+        Create by <a href="https://nakatai.ga">Nakatai</a>.
+        <br> This software used
+        <a href="https://github.com/bestiejs/platform.js">
+            platform.js
+        </a> &nbsp;,&nbsp;
+        <a href="https://getbootstrap.jp/">
+            bootstrap
+        </a>&nbsp;and&nbsp;
+        <a href="https://ipinfo.io/">
+            ipinfo.io
+        </a>.
+        <br>
+        <a href="https://github.com/Nakatai-0322/Check-Kun">
+            GitHub Source
+        </a>&nbsp;/&nbsp;
+        <a href="https://github.com/Nakatai-0322/Check-Kun/issues">
+            Issue
+        </a>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: footerComp
+    }
+</script>
