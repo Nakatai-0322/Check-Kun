8b47138893632a496e2dfb4c60f808a18cc5bc9a
Nakatai-0322 <nakatai0322@gmail.com>
2022-10-12 19:44:59 +0900

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,8 @@ module.exports = {
 	rules: {
 		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
 		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-unused-vars": "off",
+		"no-undef": "off",
 		quotes: ["error", "double"],
 		semi: ["error", "always"],
 		indent: ["error", "tab"],
